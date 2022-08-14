d17b43d9093d0ad033575356dd5dec06a44e4756
ateshkumar <111226825+ateshkumar@users.noreply.github.com>
2022-08-14 17:07:26 +0530

    Create inbcalb.py

---
+++ inbcalb.py
@@ -0,0 +1,9 @@
+a = int(input("enter the 1st no"))
+b = int(input("enter the 2nd no"))
+print(a*b)
+c = int(a+b)
+if (a*b) > 500:
+    print("their sum is")
+    print(c)
+else:
+    print("Hello LND code is running Fine")
